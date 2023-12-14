4de8a2bbcf48291f37819a131270c8c71100e206
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-12-14 18:22:42 -0500

    Update rplidar_simpletest.py

--- examples/rplidar_simpletest.py
+++ examples/rplidar_simpletest.py
@@ -7,7 +7,7 @@ from adafruit_rplidar import RPLidar
 
 # Setup the RPLidar
 PORT_NAME = "/dev/ttyS0"
-lidar = RPLidar(None, PORT_NAME, timeout=3,logging=True)
+lidar = RPLidar(None, PORT_NAME, timeout=3)
 
 # used to scale data to fit on the screen
 max_distance = 0
