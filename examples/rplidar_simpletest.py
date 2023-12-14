bbdfe8d65b058e59569f59891b95035a6d0f3214
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-12-14 14:37:35 -0500

    Update rplidar_simpletest.py

--- examples/rplidar_simpletest.py
+++ examples/rplidar_simpletest.py
@@ -10,7 +10,8 @@ from digitalio import DigitalInOut
 
 # Setup the RPLidar
 PORT_NAME = UART
-lidar = RPLidar(None, PORT_NAME, timeout=3)
+lidar = RPLidar(None, PORT_NAME,115200, timeout=3)
+
 
 # used to scale data to fit on the screen
 max_distance = 0
