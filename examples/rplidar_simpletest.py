e9ec060a5af100e8150eaa815a2935a23261a2b9
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-12-14 16:14:51 -0500

    Update rplidar_simpletest.py

--- examples/rplidar_simpletest.py
+++ examples/rplidar_simpletest.py
@@ -9,7 +9,7 @@ import busio
 # from typing import Tuple, Dict, Any, Optional, List, Iterator, Union
 from digitalio import DigitalInOut
 import board
-
+print(board.GPIO12)
 motor_pin = DigitalInOut(board.GPIO12)
 # motor_pin.value = 12
 # Setup the RPLidar
